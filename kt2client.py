1ad9f30cc7082818acac32b337053ad4479fa150
Chris Lott <rclott@hiwaay.net>
2021-05-06 12:26:32 +0900

    Moved tests to dev branch, this is just kt2client

--- kt2client.py
+++ kt2client.py
@@ -1,69 +1,119 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 import socket
 import numpy as np
 import pickle
+import matplotlib
+matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 import sys
 import time
+import threading
+import signal
 
-HOST = '127.0.0.1'  # The server's hostname or IP address
-PORT = 65432        # The port used by the server
+import kt2
+
+HOST = '192.168.1.168'  # The server's hostname or IP address
+PORT = 8282             # The port used by the server
+
+class SIGINT_handler():
+    def __init__(self):
+        self.dead = False
+
+    def signal_handler(self, signal, frame):
+        self.dead = True
+        print('You pressed Ctrl+C!')
+        print('You pressed Ctrl+C!')
+
+    def is_dead(self):
+      return self.dead
+
+
+class KT2socket:
+  def __init__(self, host, port):
+    self.host = host
+    self.port = port
+    self.sock = None
+    self.addr = '' # will server address returnd by socket.recvfrom()
+    self.connect(host, port)
+  
+  def connect(self, host, port):
+    self.host = host
+    self.port = port
+    self.sock = None
+    self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    #self.sock.settimeout(0.100)
+    self.sock.bind(('0.0.0.0', port))
+
+  def get_datagram(self, length):
+      return self.sock.recvfrom(length)
 
 def main(argv):
 
-  depth_img_array = np.zeros((80, 320), dtype=np.int16)
-  depth_img_array[0, 0] = 3000 # this value allow imshow to initialise it's color scale
-  plt.ion()
-  fig1, ax1 = plt.subplots()
-  axim1 = ax1.imshow(depth_img_array)
-  picklesize = len(pickle.dumps(depth_img_array))
+  handler = SIGINT_handler()
+  signal.signal(signal.SIGINT, handler.signal_handler)
 
-  with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.settimeout(2.00)
-    while True:
-      try:
-        s.connect((HOST, PORT))
-      except socket.timeout as e:
-        err = e.args[0]
-        if err == 'timed out':
-          print('timed out, trying again in 2 seconds')
-          time.sleep(2)
-          continue
-        else:
-          print(e)
-          exit(0)
-      else:
-        break
-    print('Connected')
+  pkt = kt2.Packet()
+  plot_data = True
 
+  if plot_data:
+    depth_img_array = np.zeros((40, 320), dtype=np.int16)
+    depth_img_array[0, 0] = 3000 # this value allow imshow to initialise it's color scale
+    plt.ion()
+    fig1, ax1 = plt.subplots()
+    ax1.text(0.98, 0.06, f'{0:05d}',
+        bbox={'facecolor': 'white', 'alpha': 1.0, 'pad': 2},
+        verticalalignment='center', horizontalalignment='right',
+        transform=ax1.transAxes,
+        color='black', fontsize=8)
+    axim1 = ax1.imshow(depth_img_array)
+    fig1.canvas.flush_events()
 
-    #s.setblocking(True)
-    s.settimeout(0.100)
-    for i in range(500):
-      s.sendall(b'G')
-      data=b''
-      while True:
-        try: 
-          if len(data) >= picklesize: break
-          data += s.recv(picklesize-len(data))
-        except socket.timeout as e:
-          err = e.args[0]
-          if err == 'timed out':
-            print('timed out')
-            continue
+  udp = KT2socket(HOST, PORT)
+
+  #print('game over')
+  #if True: exit(0)
+
+  nframe = 0
+  nrow = 0
+  t0 = time.time()
+  queue = []
+  rowbeg = False
+  rowend = False
+  maxrows = 40
+  gulping = True
+  while gulping:
+
+    buff, addr = udp.get_datagram(646)
+    if pkt.parse(buff):
+      #print( pkt.row, len(buff) )
+      if pkt.row == 0: rowbeg = True
+      if rowbeg:
+        if pkt.row >= pkt.pack_maxrows-1: rowend = True
+        if plot_data:
+          depth_img_array[pkt.row, :] = pkt.image
+        if rowend:
+          rowbeg = False
+          rowend = False
+          nframe += 1
+          if plot_data:
+            axim1.set_data(depth_img_array)
+            if nframe % 100 == 0: 
+              print('frame#', nframe)
+              ax1.text(0.98, 0.06, f'{nframe:05d}',
+                  bbox={'facecolor': 'white', 'alpha': 1.0, 'pad': 2},
+                  verticalalignment='center', horizontalalignment='right',
+                  transform=ax1.transAxes,
+                  color='black', fontsize=8)
+            fig1.canvas.flush_events()
           else:
-            print(e)
-            exit(0)
-        else:
-          continue
-      depth_img_array = pickle.loads(data)
-      print(i, len(data), depth_img_array[0][0])
-      axim1.set_data(depth_img_array)
-      fig1.canvas.flush_events()
+            if nframe % 100 == 0: print('frame#', nframe)
+        
+    if handler.is_dead():
+      print('stopping')
+      gulping = False
+      break
   
-  print('Test Ended')
-
-
 if __name__ == "__main__":
   main(sys.argv)
+
